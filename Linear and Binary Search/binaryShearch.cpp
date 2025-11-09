90088a1ac9c1f1b4442a7f3a2bf957f49b9816f6
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-11-09 17:56:21 +0600

    added binary search

---
+++ Linear and Binary Search/binaryShearch.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int element){
+    int low, mid, high;
+    low = 0;
+    high = size - 1;
+
+    while(low <= high){
+    mid = (low + high) / 2;
+    if(arr[mid] == element)
+        return mid;
+    if(arr[mid] < element)
+        low = mid + 1;
+    else
+        low = mid - 1;
+    }
+    return -1;
+}
+
+
+int main(){
+
+    int size;
+    cout << "Enter the size of the array => ";
+    cin >> size;
+
+    cout << "Enter the elements of the array => " << endl;
+    int arr[size];
+    for(int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+
+    int n;
+    cout << "Enter the targeted number => ";
+    cin >> n;
+
+    int result = binarySearch(arr, size, n);
+
+    cout << "index number is => " << result;
+
+    return 0;
+}
